d0c2f6ec88ce737e3779a72b1aab9b1e03c17c26
yokidara01 <aladinne.k@gmail.com>
2017-01-14 01:47:11 +0100

    exemple query

--- src/main/java/com/MiniProjetGestionDetude/controllers/ExempleController.java
+++ src/main/java/com/MiniProjetGestionDetude/controllers/ExempleController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.MiniProjetGestionDetude.entites.FicheVoeux;
 import com.MiniProjetGestionDetude.entites.Session;
+import com.MiniProjetGestionDetude.repository.FicheVoeuxRepo;
 import com.MiniProjetGestionDetude.service.ExempleService;
 
 @RestController
@@ -17,10 +19,15 @@ public class ExempleController {
 	
 	@Autowired
 	private ExempleService exempleService;
-	
+	@Autowired private FicheVoeuxRepo fvr; 
 	
 	@RequestMapping(value="/session",method=RequestMethod.GET)
 	public List<Session> addSession() {
 		return exempleService.getAll();
+	}
+	
+	@RequestMapping(value="/testquery",method=RequestMethod.GET)
+	public List<FicheVoeux> getQuery() {
+		return fvr.testQuery();
 	}
 }
--- src/main/java/com/MiniProjetGestionDetude/repository/FicheVoeuxRepo.java
+++ src/main/java/com/MiniProjetGestionDetude/repository/FicheVoeuxRepo.java
@@ -3,6 +3,7 @@ package com.MiniProjetGestionDetude.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.MiniProjetGestionDetude.entites.*;
 /**
@@ -13,5 +14,8 @@ public interface FicheVoeuxRepo extends JpaRepository<FicheVoeux, Integer>
 	
 	public List<FicheVoeux> findByidEnseingent (int id_ens);
 	 public List<FicheVoeux> findByidSemestre(int id_semestre);
+	 
+	 @Query(value="Select f from FicheVoeux f")
+	 public List<FicheVoeux> testQuery();
 	
 }
